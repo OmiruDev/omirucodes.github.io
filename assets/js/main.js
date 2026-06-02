51cd0b73e8cbaa43b93f47fc29b83c0e1f0c7afd
Omiru Ranshan <E241780@esoft.academy>
2026-06-02 19:39:37 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,258 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  if (!header) return;
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (window.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+scrollHeader();
+
+/*=============== SERVICES MODAL ===============*/
+// Get the modal
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalClose = document.querySelectorAll(".services__modal-close");
+
+// When the user clicks on the button, open the modal
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((mb, i) => {
+  mb.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalClose.forEach((mc) => {
+  mc.addEventListener("click", () => {
+    modalViews.forEach((mv) => {
+      mv.classList.remove("active-modal");
+    });
+  });
+});
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+let mixer = mixitup(".work__container", {
+  selectors: {
+    target: ".work__card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
+
+/* Link active work */
+const workLinks = document.querySelectorAll(".work__item");
+
+function activeWork(workLink) {
+  workLinks.forEach((wl) => {
+    wl.classList.remove("active-work");
+  });
+  workLink.classList.add("active-work");
+}
+
+workLinks.forEach((wl) => {
+  wl.addEventListener("click", () => {
+    activeWork(wl);
+  });
+});
+
+/*=============== SWIPER UPDATES ===============*/
+
+let swiperUpdates = new Swiper(".updates__container", {
+  spaceBetween: 24,
+  slidesPerView: 1,
+  loop: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+  },
+});
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    const navLink = document.querySelector(
+      ".nav__menu a[href*=" + sectionId + "]"
+    );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      if (navLink) navLink.classList.add("active-link");
+    } else {
+      if (navLink) navLink.classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+scrollActive();
+
+/*=============== LIGHT DARK THEME ===============*/
+const themeButton = document.getElementById("theme-button");
+const lightTheme = "light-theme";
+const iconTheme = "bx-sun";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the light-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(lightTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun";
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    lightTheme
+  );
+  themeButton.classList[selectedIcon === "bx bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+if (themeButton) {
+  themeButton.addEventListener("click", () => {
+    // Add or remove the light / icon theme
+    document.body.classList.toggle(lightTheme);
+    themeButton.classList.toggle(iconTheme);
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem("selected-theme", getCurrentTheme());
+    localStorage.setItem("selected-icon", getCurrentIcon());
+  });
+}
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  reset: true,
+});
+
+sr.reveal(`.nav__menu`, {
+  delay: 100,
+  scale: 0.1,
+  origin: "bottom",
+  distance: "300px",
+});
+
+sr.reveal(`.home__data`);
+sr.reveal(`.home__handle`, {
+  delay: 100,
+});
+
+sr.reveal(`.home__social, .home__scroll`, {
+  delay: 100,
+  origin: "bottom",
+});
+
+sr.reveal(`.about__img`, {
+  delay: 100,
+  origin: "left",
+  scale: 0.9,
+  distance: "30px",
+});
+
+sr.reveal(`.about__data, .about__description, .about__button-contact`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "right",
+  distance: "30px",
+});
+
+sr.reveal(`.qualification__content`, {
+  delay: 100,
+  scale: 0.95,
+  origin: "bottom",
+  distance: "30px",
+});
+
+sr.reveal(`.qualification__item`, {
+  delay: 100,
+  interval: 120,
+  origin: "bottom",
+  distance: "20px",
+});
+
+sr.reveal(`.skills__content`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "bottom",
+  distance: "30px",
+});
+
+sr.reveal(`.services__title, .services__button`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "top",
+  distance: "30px",
+});
+
+sr.reveal(`.work__card`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "bottom",
+  distance: "30px",
+});
+
+sr.reveal(`.updates__container`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "bottom",
+  distance: "30px",
+});
+
+sr.reveal(`.contact__info, .contact__title-info`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "left",
+  distance: "30px",
+});
+
+sr.reveal(`.contact__form, .contact__title-form`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "right",
+  distance: "30px",
+});
+
+sr.reveal(`.footer, .footer__container`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "bottom",
+  distance: "30px",
+});
